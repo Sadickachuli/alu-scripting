ae69d684e950731c32e1d6d7a831c5cc3445bcb1
Sadickachuli <m.achuli@alustudent.com>
2023-05-18 12:20:39 +0200

    New pattern

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,8 @@
 #!/usr/bin/env ruby
 #pattern for expression
 pattern = /School/
+input = "Neha loves School, that is why she is a School mentor"
+matches = input.scan(pattern)
+matches.each do |match|
+  puts match
+end
