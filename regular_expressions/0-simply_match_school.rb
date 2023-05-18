dbd2750265b4880ef5a5399fc051e32fa327d2b7
Sadickachuli <m.achuli@alustudent.com>
2023-05-18 12:36:14 +0200

    first script

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
 #pattern for expression
-pattern = /School/
-input = "Neha loves School, that is why she is a School mentor"
-matches = input.scan(pattern)
-matches.each do |match|
-  puts match
-end
+puts ARGV[0].scan(/School/).join
