9854cd1ae5e18f6ced32c7d7db37b0335c6dc9eb
Sadickachuli <m.achuli@alustudent.com>
2023-05-19 09:57:04 +0200

    Easy

--- regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #don't shout
-puts ARGV[0].scan(/[A-Z]/g).join
+puts ARGV[0].scan(/[A-Z]/).join
--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #text me 
-puts ARGV[0].scan(/\[Sender:(.*?)\],\[Receiver:(.*?)\],\[Flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[Sender:(.*?)\]\s\[Receiver:(.*?)\]\s\[Flags:(.*?)\]/).join(',')
